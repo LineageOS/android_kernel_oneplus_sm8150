683cb2f0cd467d6947260faeab3c1b5bd248939f
John Johansen <john.johansen@canonical.com>
2017-09-08 01:13:41 -0700

    UPSTREAM: apparmor: root view labels should not be under user control
    The root view of the label parse should not be exposed to user
    control.
    
    Change-Id: Ia5a593a931e73d45dc1cbd603e2009b8a03c1328
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    Acked-by: Seth Arnold <seth.arnold@canonical.com>

--- security/apparmor/label.c
+++ security/apparmor/label.c
@@ -1873,8 +1873,9 @@ struct aa_label *aa_label_strn_parse(struct aa_label *base, const char *str,
 	AA_BUG(!str);
 
 	str = skipn_spaces(str, n);
-	if (str == NULL)
+	if (str == NULL || (*str == '=' && base != &root_ns->unconfined->label))
 		return ERR_PTR(-EINVAL);
+
 	len = label_count_strn_entries(str, end - str);
 	if (*str == '&' || force_stack) {
 		/* stack on top of base */
@@ -1883,8 +1884,6 @@ struct aa_label *aa_label_strn_parse(struct aa_label *base, const char *str,
 		if (*str == '&')
 			str++;
 	}
-	if (*str == '=')
-		base = &root_ns->unconfined->label;
 
 	error = vec_setup(profile, vec, len, gfp);
 	if (error)
