931a0d931004488877d1d235124007fa890b8df6
Eric Dumazet <edumazet@google.com>
2019-01-17 11:23:42 -0800

    BACKPORT: tcp: move rx_opt & syn_data_acked init to tcp_disconnect()
    If we make sure all listeners have these fields cleared, then a clone
    will also inherit zero values.
    
    Change-Id: I92724e9956a2f0c3a44509c2f155f6044ab6ef6f
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/tcp_minisocks.c
+++ net/ipv4/tcp_minisocks.c
@@ -487,11 +487,6 @@ struct sock *tcp_create_openreq_child(const struct sock *sk,
 		tcp_init_xmit_timers(newsk);
 		newtp->write_seq = newtp->pushed_seq = treq->snt_isn + 1;
 
-		newtp->rx_opt.saw_tstamp = 0;
-
-		newtp->rx_opt.dsack = 0;
-		newtp->rx_opt.num_sacks = 0;
-
 		if (sock_flag(newsk, SOCK_KEEPOPEN))
 			inet_csk_reset_keepalive_timer(newsk,
 						       keepalive_time_when(newtp));
@@ -536,7 +531,6 @@ struct sock *tcp_create_openreq_child(const struct sock *sk,
 		tcp_ecn_openreq_child(newtp, req);
 		newtp->fastopen_req = NULL;
 		newtp->fastopen_rsk = NULL;
-		newtp->syn_data_acked = 0;
 
 		tcp_bpf_clone(sk, newsk);
 
