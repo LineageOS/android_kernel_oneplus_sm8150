bd7b874c879a293222137f6a0983f73a56434e92
Christoph Hellwig <hch@lst.de>
2020-07-23 08:08:47 +0200

    UPSTREAM: net: switch copy_bpf_fprog_from_user to sockptr_t
    Pass a sockptr_t to prepare for set_fs-less handling of the kernel
    pointer from bpf-cgroup.
    
    Change-Id: Id8fa0e0e5d52ac999cc8f1e58aded6f1c1dd79b3
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/sock.c
+++ net/core/sock.c
@@ -988,7 +988,8 @@ set_rcvbuf:
 	case SO_ATTACH_FILTER: {
 		struct sock_fprog fprog;
 
-		ret = copy_bpf_fprog_from_user(&fprog, optval, optlen);
+		ret = copy_bpf_fprog_from_user(&fprog, USER_SOCKPTR(optval),
+					       optlen);
 		if (!ret)
 			ret = sk_attach_filter(&fprog, sk);
 		break;
@@ -1009,7 +1010,8 @@ set_rcvbuf:
 	case SO_ATTACH_REUSEPORT_CBPF: {
 		struct sock_fprog fprog;
 
-		ret = copy_bpf_fprog_from_user(&fprog, optval, optlen);
+		ret = copy_bpf_fprog_from_user(&fprog, USER_SOCKPTR(optval),
+					       optlen);
 		if (!ret)
 			ret = sk_reuseport_attach_filter(&fprog, sk);
 		break;
