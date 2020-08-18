21e036001b659c99502d1e4ff7d417164d9edaaf
Peter Zijlstra <peterz@infradead.org>
2020-08-18 15:57:37 +0200

    UPSTREAM: module: Fix up module_notifier return values
    While auditing all module notifiers I noticed a whole bunch of fail
    wrt the return value. Notifiers have a 'special' return semantics.
    
    As is; NOTIFY_DONE vs NOTIFY_OK is a bit vague; but
    notifier_from_errno(0) results in NOTIFY_OK and NOTIFY_DONE has a
    comment that says "Don't care".
    
    From this I've used NOTIFY_DONE when the function completely ignores
    the callback and notifier_to_error() isn't used.
    
    Change-Id: Ibdbda4246e56b75e8641f831fb6fb6ac0b319dcc
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Reviewed-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Reviewed-by: Joel Fernandes (Google) <joel@joelfernandes.org>
    Reviewed-by: Robert Richter <rric@kernel.org>
    Acked-by: Steven Rostedt (VMware) <rostedt@goodmis.org>
    Link: https://lore.kernel.org/r/20200818135804.385360407@infradead.org

--- kernel/trace/bpf_trace.c
+++ kernel/trace/bpf_trace.c
@@ -2053,10 +2053,11 @@ static int bpf_event_notify(struct notifier_block *nb, unsigned long op,
 {
 	struct bpf_trace_module *btm, *tmp;
 	struct module *mod = module;
+	int ret = 0;
 
 	if (mod->num_bpf_raw_events == 0 ||
 	    (op != MODULE_STATE_COMING && op != MODULE_STATE_GOING))
-		return 0;
+		goto out;
 
 	mutex_lock(&bpf_module_mutex);
 
@@ -2066,6 +2067,8 @@ static int bpf_event_notify(struct notifier_block *nb, unsigned long op,
 		if (btm) {
 			btm->module = module;
 			list_add(&btm->list, &bpf_trace_modules);
+		} else {
+			ret = -ENOMEM;
 		}
 		break;
 	case MODULE_STATE_GOING:
@@ -2081,7 +2084,8 @@ static int bpf_event_notify(struct notifier_block *nb, unsigned long op,
 
 	mutex_unlock(&bpf_module_mutex);
 
-	return 0;
+out:
+	return notifier_from_errno(ret);
 }
 
 static struct notifier_block bpf_module_nb = {
