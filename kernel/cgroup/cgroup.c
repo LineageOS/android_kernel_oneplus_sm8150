61e1dd39884ea6b714ab17a2d9cad2f1fd974491
Wei Yang <richard.weiyang@linux.alibaba.com>
2020-09-25 17:11:10 +0800

    UPSTREAM: cgroup: remove redundant kernfs_activate in cgroup_setup_root()
    This step is already done in rebind_subsystems().
    
    Not necessary to do it again.
    
    Change-Id: Ia4b551c06aec63d33e9253ca5a6691993c8a43ae
    Signed-off-by: Wei Yang <richard.weiyang@linux.alibaba.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- kernel/cgroup/cgroup.c
+++ kernel/cgroup/cgroup.c
@@ -2028,7 +2028,6 @@ int cgroup_setup_root(struct cgroup_root *root, u16 ss_mask, int ref_flags)
 	BUG_ON(!list_empty(&root_cgrp->self.children));
 	BUG_ON(atomic_read(&root->nr_cgrps) != 1);
 
-	kernfs_activate(root_cgrp->kn);
 	ret = 0;
 	goto out;
 
