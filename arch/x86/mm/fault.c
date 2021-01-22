f3b3491e22b4746b3c570d92d1aae58d67ca798d
Marco Elver <elver@google.com>
2021-01-22 09:18:38 +0000

    FROMGIT: kfence: use pt_regs to generate stack trace on faults
    Instead of removing the fault handling portion of the stack trace based on
    the fault handler's name, just use struct pt_regs directly.
    
    Change kfence_handle_page_fault() to take a struct pt_regs, and plumb it
    through to kfence_report_error() for out-of-bounds, use-after-free, or
    invalid access errors, where pt_regs is used to generate the stack trace.
    
    If the kernel is a DEBUG_KERNEL, also show registers for more information.
    
    Link: https://lkml.kernel.org/r/20201105092133.2075331-1-elver@google.com
    Signed-off-by: Marco Elver <elver@google.com>
    Suggested-by: Mark Rutland <mark.rutland@arm.com>
    Acked-by: Mark Rutland <mark.rutland@arm.com>
    Cc: Alexander Potapenko <glider@google.com>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Cc: Alexander Potapenko <glider@google.com>
    Cc: Jann Horn <jannh@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Bug: 177201466
    (cherry picked from commit 54a5abe9b5d542ee71836439cc662efe178c8211
        https://github.com/hnaz/linux-mm v5.11-rc4-mmots-2021-01-21-20-10)
    Test: CONFIG_KFENCE_KUNIT_TEST=y passes on Cuttlefish
    Signed-off-by: Alexander Potapenko <glider@google.com>
    Change-Id: I3a60060b24f0efb4faee2e6c953973bc1263e8d1

--- arch/x86/mm/fault.c
+++ arch/x86/mm/fault.c
@@ -840,7 +840,7 @@ no_context(struct pt_regs *regs, unsigned long error_code,
 		return;
 
 	/* Only not-present faults should be handled by KFENCE. */
-	if (!(error_code & X86_PF_PROT) && kfence_handle_page_fault(address))
+	if (!(error_code & X86_PF_PROT) && kfence_handle_page_fault(address, regs))
 		return;
 
 	/*
