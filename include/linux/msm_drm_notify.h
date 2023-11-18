31d132221bda3484f966172eb5096924f8666a5a
Georg Veichtlbauer <georg@vware.at>
2023-11-18 17:37:22 +0100

    drivers: input: Add OnePlus FP drivers
    Change-Id: I1092221b0a8842997930c9c15ff9035d39a9442c

--- include/linux/msm_drm_notify.h
+++ include/linux/msm_drm_notify.h
@@ -20,6 +20,9 @@
 /* A hardware display blank early change occurred */
 #define MSM_DRM_EARLY_EVENT_BLANK		0x02
 
+/* event for onscreenfingerprint scene */
+#define MSM_DRM_ONSCREENFINGERPRINT_EVENT	0x10
+
 enum {
 	/* panel: power on */
 	MSM_DRM_BLANK_UNBLANK,
