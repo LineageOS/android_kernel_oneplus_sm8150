7b31a70e1ad997ad4c26e4a36290b7b047b04317
Michael Bestas <mkbestas@lineageos.org>
2026-08-30 02:43:47 +0300

    techpack: audio: wcd_cpe: Fix -Wimplicit-enum-enum-cast
    Change-Id: Ie415813c248c86c162673ea932927ffd1d35755d

--- techpack/audio/asoc/codecs/wcd_cpe_services.c
+++ techpack/audio/asoc/codecs/wcd_cpe_services.c
@@ -995,7 +995,7 @@ static enum cpe_svc_result broadcast_boot_event(
 static enum cpe_process_result cpe_boot_initialize(struct cpe_info *t_info,
 	enum cpe_svc_result *cpe_rc)
 {
-	enum cpe_process_result rc = CPE_SVC_FAILED;
+	enum cpe_process_result rc = CPE_PROC_FAILED;
 	struct cpe_svc_notification payload;
 	struct cmi_core_svc_event_system_boot *p = NULL;
 
@@ -2000,7 +2000,7 @@ enum cmi_api_result cmi_send_msg(void *message)
 		      GFP_ATOMIC);
 	if (!msg) {
 		CPE_SVC_REL_LOCK(&cpe_d.cpe_api_mutex, "cpe_api");
-		return CPE_SVC_NO_MEMORY;
+		return CMI_API_NO_MEMORY;
 	}
 
 	if (CMI_HDR_GET_OBM_FLAG(hdr) == CMI_OBM_FLAG_OUT_BAND)
@@ -2015,7 +2015,7 @@ enum cmi_api_result cmi_send_msg(void *message)
 	if (!msg->payload) {
 		kfree(msg);
 		CPE_SVC_REL_LOCK(&cpe_d.cpe_api_mutex, "cpe_api");
-		return CPE_SVC_NO_MEMORY;
+		return CMI_API_NO_MEMORY;
 	}
 
 	msg->address = 0;
