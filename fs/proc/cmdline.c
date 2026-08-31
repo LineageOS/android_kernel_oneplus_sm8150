e4676f0a4a9bfd9a9e03d5df5f22432b745a5f71
ShevT <evgeny.shishkov@gmail.com>
2026-08-31 11:56:08 +0300

    fs: proc: cmdline: Guard offset_addr and proc_command_line_tail
     Fixup:
    fs: proc: Add PROC_CMDLINE_APPEND_ANDROID_FORCE_NORMAL_BOOT
    
    Change-Id: Iacc2c72065d392c58eb3deea22907a284c9b4c35

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -22,8 +22,12 @@
 static char proc_command_line[COMMAND_LINE_SIZE];
 
 static void proc_command_line_init(void) {
+#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
 	char *offset_addr;
+#endif
+#ifdef CONFIG_PROC_CMDLINE_APPEND_ANDROID_FORCE_NORMAL_BOOT
 	char *proc_command_line_tail;
+#endif
 
 	strcpy(proc_command_line, saved_command_line);
 
@@ -32,7 +36,6 @@ static void proc_command_line_init(void) {
 	if (offset_addr)
 		memcpy(offset_addr, INITRAMFS_STR_REPLACE, INITRAMFS_STR_LEN);
 #endif
-
 
 #ifdef CONFIG_PROC_CMDLINE_APPEND_ANDROID_FORCE_NORMAL_BOOT
 	if (strstr(saved_command_line, INITRAMFS_STR_FIND)) {
